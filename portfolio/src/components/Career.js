284b5b63611edc8710be2181e0960fc2e7cebd90
srk9110 <srk911028@gmail.com>
2023-08-28 23:05:12 +0900

    career contents add, style fix

--- portfolio/src/components/Career.js
+++ portfolio/src/components/Career.js
@@ -6,11 +6,24 @@ function Career(props){
             <h2>CAREER</h2>
             <div className="contents-wrap">
                 <div className="content">
-                    <div className="logo"/>
-                    <div className="line"/>
-                    <div className="info-wrap">
-                        <h3>(주)Linco</h3>
+                    <div className="logo-wrap" data-aos="fade-up-right">
+                        <div className="logo"/>
+                    </div>
+                    <div className="info-wrap" data-aos="fade-up-left" data-aos-delay="500">
+                        <h3 className="company">(주) Linco</h3>
                         <div className="date">2021.07 ~ 2023.08</div>
+                        <div className="info">
+                            <div className="title">프론트엔드 개발 및 유지보수</div>
+                            <ul>
+                                <li>자체 서비스 및 외주 프로젝트 프론트엔드 퍼블리싱과 개발 진행</li>
+                                <li>추가 기능 개발 및 유지보수 진행</li>
+                            </ul>
+                            <div className="title">자체 서비스(코코리뷰) 문의 대응</div>
+                            <ul>
+                                <li>코코리뷰 위젯 설치지원 및 문의 대응</li>
+                                <li>웹 크롤링(python - selenium)을 통한 리뷰 컨텐츠 수집 및 관련 이슈와 문의 대응</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
--- portfolio/src/layouts/Footer.js
+++ portfolio/src/layouts/Footer.js
@@ -3,21 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {goUrl} from '../utils/goUrl';
 
-function Up() {
+function Footer() {
 
     return (
         <article id="footer" className="common">
             <div className="profile-wrap">
+                <div className="git icon" onClick={() => {goUrl("https://github.com/srk9110")}}/>
                 <div className="mail icon">
                     <a href="mailto:srk911028@gmail.com">
                         <FontAwesomeIcon icon={faEnvelope} style={{color: "#000000",}} />
                     </a>
                 </div>
-                <div className="git icon" onClick={() => {goUrl("https://github.com/srk9110")}}>
-                </div>
             </div>
+            <div className="right">© 2023. Kang Sae Rom. All rights reserved.</div>
         </article>
     )
 }
 
-export default Up;
+export default Footer;
